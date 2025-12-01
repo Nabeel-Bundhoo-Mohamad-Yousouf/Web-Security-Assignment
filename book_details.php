b320fc8e0a1e7a42038506a728f5def74fce2b10
Molaoa Nkalimo Kabelo <nkalimo.molaoa@umail.uom.ac.mu>
2025-12-01 14:42:01 +0400

    Refactor book_details.php for clarity and SEO
    Refactored book_details.php to improve readability and maintainability. Updated meta tags for better SEO and removed unnecessary comments.

--- book_details.php
+++ book_details.php
@@ -13,36 +13,6 @@ if ($_SERVER ["REQUEST_METHOD"] == "GET") {
     setcookie("last_genre", $genre, time()+ (10*24*60*60), "/");
 
     if (!empty($search)) {
-
-/*
-DELIMITER $$
-
-CREATE PROCEDURE book_preview_search (IN search_ID INT)
-BEGIN
-    -- First query: Book details with aggregate ratings
-    SELECT 
-        b.*, 
-        COUNT(r.rating) AS rating_num, 
-        AVG(r.rating) AS avg_rating
-    FROM book AS b 
-    LEFT JOIN review AS r ON b.book_ID = r.book_ID 
-    WHERE b.book_ID = search_ID 
-    GROUP BY b.book_ID;
-    
-    -- Second query: Individual reviews for the book
-    SELECT 
-        r.review, 
-        r.description, 
-        r.date, 
-        c.customer_name
-    FROM review AS r
-    JOIN customer AS c ON r.customer_ID = c.customer_ID
-    WHERE r.book_ID = search_ID;
-END $$
-
-DELIMITER ;
- */
-
         $statement_prepd = $conn->prepare("CALL book_preview_search(?)");
         $statement_prepd -> execute([$search]);
         $result = $statement_prepd->fetchAll(PDO::FETCH_ASSOC);
@@ -57,21 +27,19 @@ if (!empty($result)){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title><?php echo $row["title"]. " by " .$row["author"]. " | bibliohaha"?></title>
-<!--https://gist.github.com/david-bakin/255660af79c386460cdf0ee6a2a96291?permalink_comment_id=3540313 -->
-<!--https://nxdigitalagency.com/en/blog-en/seo-optimisation-of-product-cards-in-an-online-store-step-by-step-guide-->
+    <title><?php echo $row["title"]. " by " .$row["author"]. " | Bibliohaha"?></title>
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name = "keywords" content="">
-    <meta name = "description" content="Buy & Rent <?php echo $row["title"]. " by " .$row["author"]?> at the best and affordable prices. Read customer reviews.">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name = "robots" content="index, archive, follow">
-    <link rel="stylesheet" href="css/external_style.css"> 
+
+    <meta name = "description" content="<?php echo $row["title"]. " by " .$row["author"]?> - available to buy & rent at affordable prices. Fast delivery and best customer reviews.">
+    <meta name = "robots" content="index, follow">
+
+    <link rel="preload" href="../css/external_style.css" as="style">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous"> 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 
 </head>
 <body>
@@ -225,12 +193,10 @@ if (!empty($result)){
         </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js" integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y" crossorigin="anonymous"></script>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous" defer></script>
     <!--Include footer.html-->
     <?php include("includes/../footer.html")?>
 </body>
 </html>
+
 

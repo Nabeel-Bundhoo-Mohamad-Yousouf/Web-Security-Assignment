3050eac9c48c16227b8282508073691d7f61b62d
Molaoa Nkalimo Kabelo <nkalimo.molaoa@umail.uom.ac.mu>
2025-12-07 23:33:00 +0400

    Refactor header structure and breadcrumb navigation

--- includes/header.php
+++ includes/header.php
@@ -46,46 +46,43 @@ function create_breadcrumbs() {
 }
 ?>
 
-<!DOCTYPE html>
-<body>
-    <header class="sticky-top  navbar-expand-lg">
-        <div class="container-fluid">
-            <!--Logo-->
-            <a href="/" class="logo">Bibliohaha</a>
+<header class="sticky-top  navbar-expand-lg">
+    <div class="container-fluid">
+        <!--Logo-->
+        <a href="/" class="logo">Bibliohaha</a>
 
-            <!--Links-->
-            <nav class="navbar">
-                <ul>
-                    <li><a href="index.php" class="<?php echo ($activemenu=='home') ? 'active':''; ?>"> home </a></li>
-                    <li><a href="contact&about.php" class="<?php echo ($activemenu=='contact&about') ? 'active':''; ?>">about & contact</a></li>
-                </ul>
-            </nav>
-            <!--btns-->
-            <div class="icons">
-                <a class="owner_access " href="owner_dashboard.php"><i class="bi bi-sliders"> owner access</i></a>
-                <a href="login.php" class="<?php echo ($activemenu=='login') ? 'active':''; ?>" ><i class="bi bi-person"></i></a>
-                <a href="shopcart.php" class="<?php echo ($activemenu=='cart') ? 'active':''; ?>"> <i class="bi bi-cart2"></i> </a>
-            </div>
+        <!--Links-->
+        <nav class="navbar">
+            <ul>
+                <li><a href="index.php" class="<?php echo ($activemenu=='home') ? 'active':''; ?>"> home </a></li>
+                <li><a href="contact&about.php" class="<?php echo ($activemenu=='contact&about') ? 'active':''; ?>">about & contact</a></li>
+            </ul>
+        </nav>
+        <!--btns-->
+        <div class="icons">
+            <a class="owner_access " href="owner_dashboard.php"><i class="bi bi-sliders"> owner access</i></a>
+            <a href="login.php" class="<?php echo ($activemenu=='login') ? 'active':''; ?>" ><i class="bi bi-person"></i></a>
+            <a href="shopcart.php" class="<?php echo ($activemenu=='cart') ? 'active':''; ?>"> <i class="bi bi-cart2"></i> </a>
         </div>
-    </header>
+    </div>
+</header>
 
-    <div>
-        <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-                <?php
-                //Display breadcrumbs on pages except home page.
-                if (basename($_SERVER["PHP_SELF"]) !== "index.php") {
-                    $breadcrumbs = create_breadcrumbs();
-                    foreach ($breadcrumbs as $crumb){
-                        if ($crumb['url']) {
-                            echo '<li class="breadcrumb-item"><a style="text-decoration: none; color: #8e44ad;" href="' .$crumb['url']. '">' .$crumb['label']. '</a></li>';
-                        } else {
-                            echo '<li class="breadcrumb-item active" aria-current="page">' .$crumb['label']. '</li>';
-                        }
+<div>
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <?php
+            //Display breadcrumbs on pages except home page.
+            if (basename($_SERVER["PHP_SELF"]) !== "index.php") {
+                $breadcrumbs = create_breadcrumbs();
+                foreach ($breadcrumbs as $crumb){
+                    if ($crumb['url']) {
+                        echo '<li class="breadcrumb-item"><a style="text-decoration: none; color: #8e44ad;" href="' .$crumb['url']. '">' .$crumb['label']. '</a></li>';
+                    } else {
+                        echo '<li class="breadcrumb-item active" aria-current="page">' .$crumb['label']. '</li>';
                     }
                 }
-                ?>
-            </ol>
-            </nav>
-    </div>
-</body>
+            }
+            ?>
+        </ol>
+        </nav>
+</div>
